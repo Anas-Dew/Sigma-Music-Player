3eec1a68bfd481840fb72cbd6803a5816520468a
Anas Raza <96536838+Anas-Dew@users.noreply.github.com>
2022-02-10 17:01:32 +0530

    Bugs fixed
    Some bugs are fixed and meow button now works fine.

--- Music_Player/player.py
+++ Music_Player/player.py
@@ -1,10 +1,13 @@
-from ctypes import alignment
-from fileinput import filename
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
+from tkinter import messagebox
 from pygame import mixer
 import webbrowser
+from time import sleep
+import os
+os.chdir('Music_Player')
+
 
 mixer.init()
 root = Tk()
@@ -13,9 +16,9 @@ height = 170
 root.geometry(f"{width}x{height}")
 root.resizable(False, False)
 
-root.title("Sigma Music PLayer")
-
-
+root.title("Sigma Music Player")
+mixer.music.load("Sigmaintro.mp3")
+mixer.music.play()
 bottom_plate = Label(text="Anas-Dew", bg="Black",
                      fg="White", font="sans 9 italic")
 bottom_plate.pack(side=BOTTOM, fill=X)
@@ -35,11 +38,6 @@ def select_file():
         initialdir='/',
         filetypes=filetypes)
     print("File Path : " + mfile)
-    with open(mfile) as music:
-        m = music.name[35:150].split(" ")
-        main_title = m[0]
-
-        music.close()
     mixer.music.load(mfile)
     mixer.music.play()
     title_bar.config(text="Playing...")
@@ -64,33 +62,37 @@ def Resume():
 def menu_bar():
     menubar = Menu(root)
 
+    def loop():
+        messagebox.showwarning("Alert","This feature is in development.")
+
     # Adding File Menu and commands
     file = Menu(menubar, tearoff=0)
     menubar.add_cascade(label='File', menu=file)
     file.add_command(label='Open...', command=lambda: select_file())
-    file.add_command(label='Play', command=lambda: select_file.playit())
+    file.add_command(label='Replay', command=lambda: mixer.music.play())
+    file.add_checkbutton(label='Loop',command=loop)
     file.add_separator()
     file.add_command(label='Exit', command=root.destroy)
 
     # Adding more Menu and commands
-    def open_about():
-        top = Toplevel(root)
-        top.geometry("180x135")
-        top.title("Developer's Note")
-        Label(top, text="Have you liked this\n little project ? Have your\n feedback/Suggest on Github\n and follow up there for \nmore future big projects\n",
-              anchor="w", font=('Sans 10')).pack()
-        bottom_plate = Label(top, text="Anas-Dew", bg="Black",
-                             fg="White", font="sans 9 italic")
-        bottom_plate.pack(side=BOTTOM, fill=X)
-
     more = Menu(menubar, tearoff=0)
     menubar.add_cascade(label='More', menu=more)
-    more.add_command(label='Source Code', command=lambda: webbrowser.open_new(
+    more.add_command(label='Source Code',command=lambda: webbrowser.open_new(
         r"https://github.com/Anas-Dew/Sigma-Music-Player"))
-        
-    more.add_command(label='Meow !!', command=None)
+
+    def meow():
+       user_choice = messagebox.askyesno("Feedback","Did you like this app ?")
+       print(user_choice)
+       if user_choice == True:
+           rate_choice = messagebox.askyesno("Support","Rate my project on Github.")
+           if rate_choice == True:
+               messagebox.showinfo("Redirect","You are being redirected to Github.")
+               webbrowser.open_new(
+        r"https://github.com/Anas-Dew/Sigma-Music-Player")
+           
+    more.add_command(label='Meow !!', command=meow)
     more.add_separator()
-    more.add_command(label='About', command=open_about)
+    more.add_command(label='About', command=lambda: messagebox.showinfo("Developer's Note","Have you liked this little project ? Give your feedback/suggestion on Github and follow up there for more future big projects."))
     root.config(menu=menubar)
 
 
